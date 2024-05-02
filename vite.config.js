69b04ab2a0389eef299f3247ebcce7cede3ac115
Medick_ph <dk250910@gmail.com>
2024-05-02 13:35:12 +0300

    Добавил прелоад

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Preload from 'vite-plugin-preload'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Preload({
+      fonts: ['https://fonts.googleapis.com/css2?family=Neucha&display=swap', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'],
+      components: ['Main_block.vue'],
+      images: ['../assets/1.webp']
+    })
+  ],
 })
