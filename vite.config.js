8aeb5dfdd7eb22ce9c43ae74f51587c3d810228d
Medick_ph <dk250910@gmail.com>
2024-05-13 15:02:38 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     vue(),
     Preload({
-      fonts: ['https://fonts.googleapis.com/css2?family=Neucha&display=swap', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'],
+      // fonts: ['https://fonts.googleapis.com/css2?family=Neucha&display=swap', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'],
       components: ['Main_block.vue'],
       images: ['../assets/1.webp', '../assets/2.webp']
     })
